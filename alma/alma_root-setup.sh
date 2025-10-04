8a73e9e4f1904927b735d7ee8ddb6ad09d5421c4
Connor-Lock05 <monkeynutz1708@gmail.com>
2025-10-04 11:44:14 +0100

    fix: add yes flag to enable fd repo

--- alma/alma_root-setup.sh
+++ alma/alma_root-setup.sh
@@ -57,7 +57,7 @@ info "Installing System Software"
 sleep 2
 
 dnf install epel-release -y
-dnf copr enable tkbcopr/fd  # Enable fd package from Fedora cobr
+dnf copr enable tkbcopr/fd -y  # Enable fd package from Fedora cobr
 dnf install gnupg git python3-devel vim shellcheck tmux ripgrep fd multitail tree jq rsync fzf -y
 dnf install ca-certificates traceroute curl wget httpie -y
 dnf install neofetch htop sl bat hyperfine eza mariadb-server nginx -y
