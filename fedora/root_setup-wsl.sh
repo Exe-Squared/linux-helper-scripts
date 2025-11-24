cfcb97871d25cd93dd54358fb5fe2e383e6c9802
Martin Allen <47000116+martinallen-exe@users.noreply.github.com>
2025-11-24 11:12:39 +0000

    Update root_setup-wsl.sh
    merging dnf install into singe line

--- fedora/root_setup-wsl.sh
+++ fedora/root_setup-wsl.sh
@@ -59,9 +59,7 @@ dnf update -y
 info "Installing System Software"
 sleep 2
 
-dnf install gcc make automake autoconf gnupg git python3-devel tree jq yq rsync fzf -y
-dnf install ca-certificates traceroute curl wget redis mkcert -y
-dnf install htop mariadb-server httpd mod_ssl golang-bin postgresql postgresql-server -y
+dnf install gcc make automake autoconf gnupg git python3-devel tree jq yq rsync fzf ca-certificates traceroute curl wget redis mkcert htop mariadb-server httpd mod_ssl golang-bin postgresql postgresql-server -y
 
 systemctl enable --now httpd
 systemctl enable --now mariadb
